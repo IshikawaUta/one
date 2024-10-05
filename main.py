ad5c38305467dc3bab1946a02434572fd224da51
IshikawaUta <jayaj7129@gmail.com>
2024-10-05 15:15:02 +0700

    add repository

---
+++ main.py
@@ -0,0 +1,24 @@
+#variable
+angka1 = 10
+angka2 = 5
+angka3 = 1
+
+#operasi penjumlahan
+hasil_penjumlahan = angka1 + angka2 + angka3
+print("Hasil penjumlahan:", hasil_penjumlahan)
+
+#operasi pengurangan
+hasil_pengurangan = angka1 - angka2 - angka3
+print("Hasil penjumlahan:", hasil_pengurangan)
+
+#operasi perkaklian
+hasil_perkalian = angka1 * angka2 * angka3
+print("Hasil penjumlahan:", hasil_perkalian)
+
+#operasi pembagian
+hasil_pembagian = angka1 / angka2 / angka3
+print("Hasil penjumlahan:", hasil_pembagian)
+
+#operasi pemangkatan
+hasil_pemangkatan = angka1 ** angka2 ** angka3
+print("Hasil penjumlahan:", hasil_pemangkatan)
---
+++ main1.py
@@ -0,0 +1,7 @@
+nilai1 = 15
+nilai2 = 10
+nilai3 = 20
+
+print(nilai2 == nilai3)
+print(nilai1 >= nilai3)
+print(nilai1 < nilai3)
